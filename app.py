4473253d5c3ce288102f8af231090afeb67f956f
Onelalwine <zyming-one@outlook.com>
2026-07-08 00:15:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,26 @@ import os
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "static/uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    image_path = None
+    image = None
 
     if request.method == "POST":
         file = request.files.get("image")
 
-        if file and file.filename != "":
-            save_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        if file and file.filename:
+            filename = file.filename
+            save_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(save_path)
-            image_path = save_path
 
-    return render_template("index.html", image=image_path)
+            image = "/" + save_path.replace("\\", "/")
+
+    return render_template("index.html", image=image)
 
 
 if __name__ == "__main__":
